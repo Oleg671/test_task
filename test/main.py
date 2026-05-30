01bbbb1c5a18641dc8fee82e418f06ab785b3fd3
Samuel Satou <oliveryuto@gmail.com>
2026-05-30 18:37:29 +0300

    Add comments

--- test/main.py
+++ test/main.py
@@ -1,7 +1,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        header = self.headers.get('X-Forwarded-For', 'not found')
+        header = self.headers.get('X-Forwarded-For', 'not found') #not found для перестраховки и отладки
         response = f"X-Forwarded-For: {header}\n"
         self.send_response(200)
         self.send_header('Content-Type', 'text/plain')
