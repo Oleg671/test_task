189b1f7189a2ffcb072939d3275680ae5c945997
root <oliveryuto@gmail.com>
2026-05-30 15:16:26 +0000

    Base of test task

---
+++ test/main.py
@@ -0,0 +1,10 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        header = self.headers.get('X-Forwarded-For', 'not found')
+        response = f"X-Forwarded-For: {header}\n"
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(response.encode())
+HTTPServer(('0.0.0.0', 8080), Handler).serve_forever() #без роутов ибо зачем
